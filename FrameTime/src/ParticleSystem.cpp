d64d6612fde0ce7b1f9500b1bbb68a37c0717f62
David Wicks <david@sosolimited.com>
2015-04-30 12:45:18 -0400

    Update default gitignore with xcode ignores.

---
+++ FrameTime/src/ParticleSystem.cpp
@@ -0,0 +1,10 @@
+//
+//  ParticleSystem.cpp
+//
+//  Created by Soso Limited on 4/30/15.
+//
+//
+
+#include "ParticleSystem.h"
+
+using namespace FrameTime;
---
+++ FrameTime/src/ParticleSystem.h
@@ -0,0 +1,20 @@
+//
+//  ParticleSystem.h
+//
+//  Created by Soso Limited on 4/30/15.
+//
+//
+
+#pragma once
+
+namespace FrameTime {
+
+class ParticleSystem
+{
+public:
+
+private:
+
+};
+
+} // namespace FrameTime
