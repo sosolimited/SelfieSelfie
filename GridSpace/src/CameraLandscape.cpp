5343a703c5b1d3c259fc4e79f7c9b2085cd862f3
David Wicks <david@sosolimited.com>
2015-05-18 14:46:41 -0400

    Textured teapot in the distance.

--- GridSpace/src/CameraLandscape.cpp
+++ GridSpace/src/CameraLandscape.cpp
@@ -12,7 +12,9 @@ using namespace cinder;
 
 void CameraLandscape::setup()
 {
-
+	auto shader = gl::getStockShader( gl::ShaderDef().texture( GL_TEXTURE_2D ).color() );
+	auto mat = glm::translate( vec3( 20, 0, 20 ) ) * glm::scale( vec3( 20 ) ) * glm::rotate<float>( M_PI / 4, vec3( 0, 1, 0 ) );
+	batch = gl::Batch::create( geom::Teapot().subdivisions( 8 ) >> geom::Transform( mat ), shader );
 }
 
 void CameraLandscape::updateTexture( const ci::Surface &iImage )
@@ -27,17 +29,9 @@ void CameraLandscape::updateTexture( const ci::Surface &iImage )
 
 void CameraLandscape::draw() const
 {
-
 	if( texture && batch )
 	{
 		gl::ScopedTextureBind tex0( texture, 0 );
 		batch->draw();
-
-	}
-
-	if( texture ) {
-		gl::ScopedModelMatrix mat;
-		gl::translate( vec3( 0, 0, 50.0f ) );
-		gl::draw( texture );
 	}
 }
