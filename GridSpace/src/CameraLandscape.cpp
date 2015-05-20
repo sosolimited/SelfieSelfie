a7533f1f1c7818010985d42878d66292413d7551
David Wicks <david@sosolimited.com>
2015-05-20 12:55:04 -0400

    Cancel slitscan effect.

--- GridSpace/src/CameraLandscape.cpp
+++ GridSpace/src/CameraLandscape.cpp
@@ -105,7 +105,9 @@ void CameraLandscape::updateTexture( const ci::Surface &iImage )
 		texture = gl::Texture::create( iImage, gl::Texture::Format().loadTopDown() );
 	}
 	else {
-//		texture->update( iImage, 0, ivec2( 0, line ) );
+		// TODO: store all frames in texture, but offset them as we go.
+		texture->update( iImage, 0, ivec2( 0, line ) );
+		/*
 		gl::ScopedTextureBind tex( texture );
 		auto x_offset = 0;
 		auto y_offset = line;
@@ -116,6 +118,7 @@ void CameraLandscape::updateTexture( const ci::Surface &iImage )
 
 		glTexSubImage2D( texture->getTarget(), 0, x_offset, y_offset, iImage.getWidth(), 1, dataFormat, type, data_ptr );
 		line = (line + 1) % texture->getHeight();
+		*/
 	}
 }
 
