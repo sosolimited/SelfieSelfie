50a4eb9e58cb888dedbdd33fb13b5612ac8ccd6f
David Wicks <wicks.db@gmail.com>
2015-06-02 15:41:41 -0400

    Remove android studio project from GridSpace.
    It was not working for us, and added bunches of unnecessary files.

--- GridSpace/androidstudio/GridSpace/app/src/main/java/com/sosolimited/gridspace/gridspace/GridSpaceActivity.java
+++
@@ -1,7 +0,0 @@
-package com.sosolimited.gridspace.gridspace;
-
-import android.app.NativeActivity;
-
-public class GridSpaceActivity extends NativeActivity {
-    static final String TAG = "GridSpaceActivity";
-}
