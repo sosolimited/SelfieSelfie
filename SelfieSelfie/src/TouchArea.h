be0cb432a7824cc15cda0729dbe45f36ff0662c7
David Wicks <david@sosolimited.com>
2015-06-10 16:15:03 -0400

    Stub class for button-like selection.

---
+++ SelfieSelfie/src/TouchArea.h
@@ -0,0 +1,46 @@
+//
+//  TouchArea.h
+//
+//  Created by Soso Limited on 6/10/15.
+//
+//
+
+#pragma once
+
+namespace soso {
+
+///
+/// A rectangle that calls a function on tap.
+/// No coordination between touch areas.
+///
+class TouchArea
+{
+public:
+	~TouchArea()
+	{
+		disconnect();
+	}
+
+	static std::shared_ptr<TouchArea> create( const ci::Rectf &iBounds, const std::function<void ()> &iCallback );
+
+private:
+	ci::Rectf								bounds;
+	std::function<void ()>	callback;
+	uint32_t								trackedTouch = 0;
+	bool										wasInside = false;
+	ci::signals::Connection	touchBeginConnection, touchEndConnection;
+
+	void touchBegin( const ci::app::TouchEvent &iEvent );
+	void touchEnd( const ci::app::TouchEvent &iEvent );
+	void connect();
+	void disconnect();
+
+	TouchArea( const ci::Rectf &iBounds, const std::function<void ()> &iCallback )
+	: bounds( iBounds ),
+		callback( iCallback )
+	{
+		connect();
+	}
+};
+
+} // namespace soso
